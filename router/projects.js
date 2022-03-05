6672925a72d815e602b79d8d36ad307894c2adeb
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-03-05 18:14:18 +0200

    Update projects.js

--- router/projects.js
+++ router/projects.js
@@ -10,7 +10,7 @@ const color = require('colors')
 
 router.get('/order-summery', async (req, res) => {
     try {
-        res.sendFile(path.resolve(__dirname, '../public/order-summary/index.html'))
+        res.sendFile(path.join(__dirname, '../public/order-summary/index.html'))
     } catch (err) {
         console.log(color.bgRed.black(err))
     }
